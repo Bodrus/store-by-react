7bd8dd4ba8cf21ca560124200d8a9ab6f8063237
Maxim.Bodrus <maxshop2010@gmail.com>
2019-07-23 00:16:58 +0300

    Updates

---
+++ precache-manifest.a5ec17897ed75ec9a9ace7742c5b6f67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1ad443cfdf2f63c12033fd6b530310",
+    "url": "/store-by-react/index.html"
+  },
+  {
+    "revision": "ddf11bdb0de42b4206c6",
+    "url": "/store-by-react/static/css/main.8d554285.chunk.css"
+  },
+  {
+    "revision": "0a06750a2854d39f28ae",
+    "url": "/store-by-react/static/js/2.d0fa09a9.chunk.js"
+  },
+  {
+    "revision": "ddf11bdb0de42b4206c6",
+    "url": "/store-by-react/static/js/main.7a7305ec.chunk.js"
+  },
+  {
+    "revision": "1cb2276e732d6f7b5554",
+    "url": "/store-by-react/static/js/runtime~main.a6594f92.js"
+  },
+  {
+    "revision": "25b5e3e158ad36f5f64ae04d1235e0aa",
+    "url": "/store-by-react/static/media/error.25b5e3e1.png"
+  },
+  {
+    "revision": "6ba930b2b3a7f6fe8c0d6a9b77609bae",
+    "url": "/store-by-react/static/media/logo.6ba930b2.png"
+  },
+  {
+    "revision": "f228fdd55ec7477271dee51823570651",
+    "url": "/store-by-react/static/media/notImage.f228fdd5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/store/precache-manifest.85653d27f5b5eb61a80f318854156046.js"
+  "/store-by-react/precache-manifest.a5ec17897ed75ec9a9ace7742c5b6f67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/store/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/store-by-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
